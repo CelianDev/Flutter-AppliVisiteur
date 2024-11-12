921c66a9982f0b7b253ab308598c769e08b7956f
Nitrozer <147092043+Nitrozer@users.noreply.github.com>
2024-11-12 17:04:02 +0100

    #APP-1 ajout de l'interface de login

---
+++ lib/src/login/login_view.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class LoginView extends StatefulWidget {
+  static const routeName = '/login';
+
+  const LoginView({super.key});
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _usernameController,
+              decoration: const InputDecoration(labelText: 'Username'),
+            ),
+            const SizedBox(height: 16),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            const SizedBox(height: 24),
+            ElevatedButton(
+              onPressed: () {
+                // Ajoutez votre logique d'authentification ici
+                Navigator.of(context).pushReplacementNamed('/');
+              },
+              child: const Text('Se connecter'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+}
